220f1bb3c39e9e0ab2901a9f72a112f95806d8cb
Philip Martins <philip.martt@outlook.com>
2023-04-10 01:06:02 +0000

    setting bucket to private

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -8,3 +8,13 @@ resource "aws_s3_bucket" "raw-bucket" {
         environment = "dev"
     }
 }
+
+# setting the bucket to private
+resource "aws_s3_bucket" "block_public_1" {
+    bucket = aws_s3_bucket.raw-bucket.id
+
+    block_public_acls = true
+    block_pulic_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
